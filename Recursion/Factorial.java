702ac510370a820e590ea518869ca192ecc5f39a
pearlaggarwal <aggarwalpearl156@gmail.com>
2025-08-27 23:18:06 +0530

    resursion

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,1 +1,17 @@
 package Recursion;
+
+public class Factorial{
+    static int fact(int n){
+        if(n==0||n==1)
+            return 1;
+        int x=n*fact(n-1);
+        return x;
+    }
+    public static void main(String[] args) {
+        int n=3;
+        int m=4;
+        int x=1;
+        System.out.println(fact(n));
+    }
+
+}
