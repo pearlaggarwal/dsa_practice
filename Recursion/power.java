fbfc9f8e7e5e089b2afe29f35df44fb742789919
pearlaggarwal <aggarwalpearl156@gmail.com>
2025-08-28 22:59:37 +0530

    recursion

--- Recursion/power.java
+++ Recursion/power.java
@@ -1,5 +1,15 @@
 package Recursion;
 
 public class power {
-    
+    static int power(int a,int b){
+        if (b==0)
+            return 1;
+        int x=power(a, b-1)*a;
+        return x;
+    }
+    public static void main(String[] args) {
+        int n=3;
+        int m=4;
+        System.out.println(power(n, m));
+    }
 }
